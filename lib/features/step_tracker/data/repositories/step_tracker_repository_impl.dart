6828d661018f0c7a755a1da7048f9e3a565740ca
Кавелич М.И. / гр.334701 <kavelichmatvey@gmail.com>
2026-04-11 12:42:02 +0300

    Исправил стрим

--- lib/features/step_tracker/data/repositories/step_tracker_repository_impl.dart
+++ lib/features/step_tracker/data/repositories/step_tracker_repository_impl.dart
@@ -1,13 +1,20 @@
+import 'dart:async';
+
 import 'package:pedometer/pedometer.dart';
 import 'package:self_bet/features/step_tracker/domain/entities/goal.dart';
 import 'package:self_bet/features/step_tracker/domain/repositories/repositories.dart';
 
 class StepTrackerRepositoryImpl implements StepTrackerRepository {
-  final Stream<Goal> _currentGoalStream;
-
   StepTrackerRepositoryImpl({
     required Stream<Goal> currentGoalStream,
   }) : _currentGoalStream = currentGoalStream;
+
+  final Stream<Goal> _currentGoalStream;
+
+  final StreamController<int> _stepCountController =
+      StreamController<int>.broadcast();
+
+  StreamSubscription<StepCount>? _pedometerSubscription;
 
   Stream<Goal> get currentGoalStream => _currentGoalStream;
 
@@ -17,15 +24,33 @@ class StepTrackerRepositoryImpl implements StepTrackerRepository {
     if (step < 0 || time.isAfter(DateTime.now())) {
       return;
     }
-    // Forward to stream/state when wiring Pedometer.stepCountStream.
+    if (!_stepCountController.isClosed) {
+      _stepCountController.add(step);
+    }
   }
-  @override
-  Stream<int> getStepCount() {
-    return Stream.value(0);
+
+  void initStepCountStream() {
+    _pedometerSubscription?.cancel();
+    _pedometerSubscription = Pedometer.stepCountStream.listen(
+      onStepCount,
+      onError: (_) {
+      },
+    );
+  }
+
+  void dispose() {
+    _pedometerSubscription?.cancel();
+    _pedometerSubscription = null;
+    _stepCountController.close();
   }
 
   @override
+  Stream<int> getStepCount() => _stepCountController.stream;
+
+  @override
   Future<Goal> getGoal() {
-    return Future.value(Goal(goal: 10000, currentSteps: 0, deadLine: DateTime.now()));
+    return Future.value(
+      Goal(goal: 10000, currentSteps: 0, deadLine: DateTime.now()),
+    );
   }
 }
