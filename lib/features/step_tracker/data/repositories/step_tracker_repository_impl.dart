217b225c9f6eca8fca90ac16f9bf1f8e4fe97a39
Кавелич М.И. / гр.334701 <kavelichmatvey@gmail.com>
2026-04-11 11:10:03 +0300

    Реализовал стрим подписку на текущее количество шагов

--- lib/features/step_tracker/data/repositories/step_tracker_repository_impl.dart
+++ lib/features/step_tracker/data/repositories/step_tracker_repository_impl.dart
@@ -1,8 +1,24 @@
+import 'package:pedometer/pedometer.dart';
 import 'package:self_bet/features/step_tracker/domain/entities/goal.dart';
 import 'package:self_bet/features/step_tracker/domain/repositories/repositories.dart';
 
 class StepTrackerRepositoryImpl implements StepTrackerRepository {
-  
+  final Stream<Goal> _currentGoalStream;
+
+  StepTrackerRepositoryImpl({
+    required Stream<Goal> currentGoalStream,
+  }) : _currentGoalStream = currentGoalStream;
+
+  Stream<Goal> get currentGoalStream => _currentGoalStream;
+
+  void onStepCount(StepCount event) {
+    final int step = event.steps;
+    final DateTime time = event.timeStamp;
+    if (step < 0 || time.isAfter(DateTime.now())) {
+      return;
+    }
+    // Forward to stream/state when wiring Pedometer.stepCountStream.
+  }
   @override
   Stream<int> getStepCount() {
     return Stream.value(0);
